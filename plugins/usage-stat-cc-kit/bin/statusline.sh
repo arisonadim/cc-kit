bb03afa3bd372e5f339811da9880beca3bb17f9c
Dmitry Ermilov <ermilov.dmitry@gmail.com>
2026-07-13 13:30:07 +0700

    [add] fix when data not available

--- plugins/usage-stat-cc-kit/bin/statusline.sh
+++ plugins/usage-stat-cc-kit/bin/statusline.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-input=$(cat)
-five_hour=$(echo "$input" | grep -o '"five_hour":{[^}]*}')
-used=$(echo "$five_hour" | grep -o '"used_percentage":[0-9.]*' | grep -o '[0-9.]*$' | head -1)
-reset=$(echo "$five_hour" | grep -o '"resets_at":[0-9]*' | grep -o '[0-9]*$' | head -1)
-used=${used:-0}
-reset=${reset:-0}
+input=$(cat | tr -d '\n')
+five_hour=$(echo "$input" | grep -o '"five_hour"[[:space:]]*:[[:space:]]*{[^}]*}')
+used=$(echo "$five_hour" | grep -o '"used_percentage"[[:space:]]*:[[:space:]]*[0-9.]*' | grep -o '[0-9.]*$' | head -1)
+reset=$(echo "$five_hour" | grep -o '"resets_at"[[:space:]]*:[[:space:]]*[0-9]*' | grep -o '[0-9]*$' | head -1)
+
+if [[ -z "$used" || -z "$reset" ]]; then
+  printf "5h left: n/a\n"
+  exit 0
+fi
+
 now=$(date +%s)
 left_sec=$(( reset - now ))
 (( left_sec < 0 )) && left_sec=0
